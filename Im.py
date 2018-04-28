c957a147fe48bfbf9ca46093205d113088a9233a
Imdad Ali <imdadali@iiitdmj.ac.in>
2018-04-29 01:14:26 +0530

    Add files via upload

--- Im.py
+++ Im.py
@@ -1,7 +1,9 @@
-import flask import Flask
+from flask import Flask
 
 app=Flask(__name__)
 
 app.route('/')
 def index():
     return 'This is the Homepage'
+if __name__== "__main__":
+    app.run(debug=True)
